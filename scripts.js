14e821b4f6f79b54e57a70c410c5539978a3ded6
akin <akin@femka.de>
2025-09-24 20:01:37 +0200

    added: script for contact form

--- scripts.js
+++ scripts.js
@@ -10,3 +10,22 @@ function hideSidebar(event){
   const sidebar = document.querySelector('.sidebar');
   sidebar.style.display = "none";
 }
+
+function sendMail(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+
+  fetch("YOUR_URL", {
+    method: "POST",
+    body: new FormData(event.target),
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then(() => {
+      window.location.href = "./send_mail.html";
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
